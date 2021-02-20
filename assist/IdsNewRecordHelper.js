51682b9942273a52b14fed081003e412afe6e2d8
Charles Ross <chivalry@mac.com>
2021-02-20 13:47:37 -0800

    Working contact add

--- assist/IdsNewRecordHelper.js
+++ assist/IdsNewRecordHelper.js
@@ -1,17 +1,17 @@
 ({
     validateContactForm: function(component) {
-        let validContact = true;
-        const allValid = component.find('contactField').reduce(function (validFields, inputCmp) {
+        var validContact = true;
+        var allValid = component.find('contactField').reduce(function (validFields, inputCmp) {
             inputCmp.showHelpMessageIfInvalid();
             return validFields && inputCmp.get('v.validity').valid;
         }, true);
         if (allValid) {
-            const account = component.get('v.newContact');
-            if ($A.util.isEmpty(account)) {
+            var account = component.get("v.newContact");
+            if($A.util.isEmpty(account)) {
                 validContact = false;
                 console.log("Quick action context doesn't have a valid account.");
             }
-            return(validContact);
+        return(validContact);
         }
-    }
+	}
 })
